36d15df03b46dfc7099b0dede9a1b7c27317fde0
Andrea To Chip <andreatochip@yahoo.com>
2025-10-01 15:24:46 +0800

    Renaming, Removing, Converting

--- Amazon Best Sellers Analysis/main.py
+++ Amazon Best Sellers Analysis/main.py
@@ -13,3 +13,12 @@ print(df.columns)
 
 # Get summary statistics for each column
 print(df.describe())
+
+# Remove all duplicate rows
+df.drop_duplicates(inplace=True)
+
+# Rename column names to "Title", "Publication Year", and "Rating"
+df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
+
+# Convert "Price" column to use the float data type
+df["Price"] = df["Price"].astype(float)
