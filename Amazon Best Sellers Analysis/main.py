dfab95e3a26732c92d87594d57a34e3650bc31bd
Andrea To Chip <andreatochip@yahoo.com>
2025-10-01 11:14:32 +0800

    Create main.py

---
+++ Amazon Best Sellers Analysis/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('bestsellers.csv')
+
+# Get the first 5 rows of the spreadsheet
+print(df.head())
+
+# Get the shape of the spreadsheet
+print(df.shape)
+
+# Get the column names of the spreadsheet
+print(df.columns)
+
+# Get summary statistics for each column
+print(df.describe())
