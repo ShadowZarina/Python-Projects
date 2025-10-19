32d5f8b4398e6270e5499be3e2a97a12225c7e7f
Andrea To Chip <andreatochip@yahoo.com>
2025-10-19 11:40:58 +0800

    Update Word Guessing Game.py

--- Word Guessing Game.py
+++ Word Guessing Game.py
@@ -35,13 +35,13 @@ while attempts > 0:
   if guess in word:
   for i in range(len(word)):
     if word[i] == guess:
-      guessedWord[i] = guess
+      guessed_word[i] = guess
       print('Great guess!')
     else:
       attempts -= 1
       print('Wrong guess! Attempts left: ' + str(attempts))
 
-    if '_' not in guessedWord:
+    if '_' not in guessed_word:
           print('\nCongratulations!! You guessed the word: ' + word)
           break
       
