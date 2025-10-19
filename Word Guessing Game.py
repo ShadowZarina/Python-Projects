1f44b5c7ffff86217cafcdf5ed661b8fc58530b0
Andrea To Chip <andreatochip@yahoo.com>
2025-10-19 11:37:52 +0800

    Create Word Guessing Game.py

---
+++ Word Guessing Game.py
@@ -0,0 +1,39 @@
+import random
+
+word_collection = [
+  "ADVENTURE",
+  "TELEPHONE",
+  "WONDERFUL",
+  "EXCELLENT",
+  "NEIGHBOR",
+  "KITCHEN",
+  "COMPUTER",
+  "HOLIDAY",
+  "ELEVATOR",
+  "SCISSORS",
+  "CHOCOLATE",
+  "PROBABLY",
+  "DIFFERENT",
+  "RESTAURANT",
+  "MAGNETIC",
+  "SUFFICIENT",
+  "CHAMPION",
+  "CABINET",
+  "GUARANTEE",
+  "IMAGINE"
+]
+
+word = random.choice(word_collection)
+
+guessed_word = ['_'] * len(word)
+attempts = 10
+
+while attempts > 0:
+  print('\nCurrent word: ' + ' '.join(guessed_word))
+  guess = char(input("Enter a letter: ").upper()
+
+  if guess in word:
+  for i in range(len(word)):
+    if word[i] == guess:
+      guessedWord[i] = guess
+  print('Great guess!')
