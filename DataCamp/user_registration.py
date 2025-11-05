e8d431828fe033403cde1740104b50ad24962ec2
Andrea To Chip <andreatochip@yahoo.com>
2025-11-05 21:31:17 +0800

    Update user_registration.py

--- DataCamp/user_registration.py
+++ DataCamp/user_registration.py
@@ -1,3 +1,24 @@
+# Re-run this cell and examine the docstring of each function
+from python_functions import validate_name, validate_email, validate_password, top_level_domains
+
+print("validate_name\n")
+print(validate_name.__doc__)
+print("--------------------\n")
+
+print("validate_email\n")
+print(validate_email.__doc__) 
+print("--------------------\n")
+
+print("validate_password\n")
+print(validate_password.__doc__)
+
+# The top level domains variable is used in validate_email to approve only certain email domains
+print(top_level_domains)
+
+'''
+
+'''
+
 def validate_user(name, email, password):
 
     if validate_name(name) == False:
