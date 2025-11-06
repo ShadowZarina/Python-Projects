c809cd6af98abb93a52b41ddff91b27c970f6a81
Andrea To Chip <andreatochip@yahoo.com>
2025-11-06 18:41:15 +0800

    Update calorie_calculator.py

--- DataCamp/calorie_calculator.py
+++ DataCamp/calorie_calculator.py
@@ -6,3 +6,26 @@ with open('nutrition.json', 'r') as json_file:
     
 # Display the first 3 items of the nutrition dictionary
 list(nutrition_dict.items())[:3]
+
+'''
+Sample Output of Nutrition Dict
+
+[('Cornstarch',
+  {'calories': 381,
+   'total_fat': 0.1,
+   'protein': 0.26,
+   'carbohydrate': 91.27,
+   'sugars': 0.0}),
+ ('Nuts, pecans',
+  {'calories': 691,
+   'total_fat': 72.0,
+   'protein': 9.17,
+   'carbohydrate': 13.86,
+   'sugars': 3.97}),
+ ('Eggplant, raw',
+  {'calories': 25,
+   'total_fat': 0.2,
+   'protein': 0.98,
+   'carbohydrate': 5.88,
+   'sugars': 3.53})]
+'''
